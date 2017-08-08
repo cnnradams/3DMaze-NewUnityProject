07bb74b6051d855749dc9da822542deeca4f6776
Stealth2994 <cnnr252@gmail.com>
2017-08-08 19:55:37 -0400

    Initial Eclipse

---
+++ src/main/Main.java
@@ -0,0 +1,18 @@
+package main;
+
+public class Main {
+
+	public String fileName;
+	public static void main(String[] args) {
+	}
+	
+	public static String getInput(String fileName) {
+		return fileName;
+		
+	}
+	
+	public static String pathFind(String file) {
+		return file;
+		
+	}
+}
